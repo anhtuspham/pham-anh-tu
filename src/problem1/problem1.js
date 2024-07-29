e65bef136af03e6e558f2f487d3db2ebbf69f469
tu pham <panhtu0902@gmail.com>
2024-07-30 04:25:57 +0700

    docs: update documentation

--- src/problem1/problem1.js
+++ src/problem1/problem1.js
@@ -1,4 +1,4 @@
-// loop
+// I use a loop to iterate through each single element in the project
 const sum_to_n_a = function (n) {
   let sum = 0;
   for (let i = 0; i <= n; i++) {
@@ -7,7 +7,7 @@ const sum_to_n_a = function (n) {
   return sum;
 };
 
-// recursive
+// I use recursion to call the function until the end (n === 0)
 const sum_to_n_b = function (n) {
   if (n === 0) {
     return 0;
@@ -16,7 +16,7 @@ const sum_to_n_b = function (n) {
   }
 };
 
-// math 
+// I just use a math formula to solve it
 const sum_to_n_c = function (n) {
   return (n * (n + 1)) / 2;
 };
